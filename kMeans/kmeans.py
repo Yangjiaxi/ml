d51c2365596aacca260348a5c791bd8e028b746f
杨家玺 <yang@MacBook-Pro.lan>
2018-04-17 17:30:06 +0800

    <MOD> add norm distance method

--- kMeans/kmeans.py
+++ kMeans/kmeans.py
@@ -36,7 +36,7 @@ def plot_res(C_set, k):
     plt.show()
 
 
-def run(data_path, k):
+def run(data_path, k, norm):
     data_set = read_data(data_path)
     m = data_set.shape[0]
     mean_vector = data_set[np.random.choice(m, k, replace=False)]
@@ -46,7 +46,8 @@ def run(data_path, k):
         for i in range(k + 1):
             C.append([])
         for j in range(m):
-            dj = ((data_set[j] - mean_vector) ** 2).sum(axis=1).argmin()
+            dj = ((np.abs((data_set[j] - mean_vector)
+                          ** norm).sum(axis=1)) ** (1 / norm)).argmin()
             C[dj].append(data_set[j].tolist())
         mean_prev = mean_vector.copy()
         for i in range(k):
@@ -59,4 +60,4 @@ def run(data_path, k):
 
 
 if __name__ == '__main__':
-    run('watermelon_4.0.txt', 1)
+    run('watermelon_4.0.txt', 3, norm=2)
